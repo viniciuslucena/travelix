beec87ca0e8aa838ce984a8d02e9e8a7d025e992
Vinicius Lucena <jviniiciuslucena@gmail.com>
2020-06-12 11:35:56 -0300

    Footer finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const header = document.querySelector('.header')
 const hamburgerMenu = document.querySelector('.hamburger-menu')
 
-hamburgerMenu.addEventListener('click', (e) => {
+window.addEventListener('scroll', (e) => {
+    let windowPosition = window.scrollY > 100
+    header.classList.toggle('active', windowPosition)
+})
+
+hamburgerMenu.addEventListener('click', () => {
     header.classList.toggle('menu-open')
 })
